ebd29bd4d639820cf3bc6ca035d6d0da2d605531
Ntare Guy <gntare2@gmail.com>
2024-06-21 16:33:08 +0200

    transition from typeORM to Prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,28 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserController } from './controllers/user/user.controller';
-import { UserService } from './services/user/user.service';
-import { UserEntity } from './entities/user.entity';
+
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      ssl: { rejectUnauthorized: false },
-      url: process.env.DATABASE_URL,
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.PORT),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    }),
-    TypeOrmModule.forFeature([UserEntity]),
-  ],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  imports: [ConfigModule.forRoot()],
+  controllers: [AppController],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
