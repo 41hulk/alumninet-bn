3a9c23996fd6d18731e14cad65eb9c348631c10f
Ntare Guy <gntare2@gmail.com>
2024-07-12 00:04:59 +0200

    fix(cmpgn): add user to campaign as creator

--- src/campaign/campaign.service.ts
+++ src/campaign/campaign.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class CampaignService {}
+export class CampaignService {
+  constructor(private prisma: PrismaService) {}
+
+  async getCampaigns() {
+    const campaigns = await this.prisma.campaign.findMany({
+      where: { deleted_at: null },
+      include: { user: true },
+      orderBy: { created_at: 'desc' },
+    });
+    return campaigns;
+  }
+}
--- src/campaign/dto/CreateCampaign.dto.ts
+++ src/campaign/dto/CreateCampaign.dto.ts
@@ -1,3 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 import { Dto } from 'src/lib/dto/Dto';
 
-export class CreateCampaignDto extends Dto<CreateCampaignDto> {}
+export class CreateCampaignDto extends Dto<CreateCampaignDto> {
+  @ApiProperty()
+  @IsString()
+  title: string;
+
+  @ApiProperty()
+  @IsString()
+  description: string;
+
+  @ApiProperty()
+  @IsString()
+  startDate: Date;
+
+  @ApiProperty()
+  @IsString()
+  endDate: string;
+
+  @ApiProperty()
+  @IsString()
+  targetAmount: string;
+}
